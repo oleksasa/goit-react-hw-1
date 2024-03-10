d0ba4affa123c0c3e8190d2f2c9f61a1f747ec82
oleksasa <oleksandrasevcuk04@gmail.com>
2024-03-10 22:52:21 +0200

    finally commit

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import FriendItem from './FriendItem/FriendItem';
+
+import s from './FriendList.module.css';
+
+const FriendList = ({ friends }) => {
+  return (
+    <div className={s.friendsList}>
+      {friends.map(friend => (
+        <FriendItem key={friend.id} {...friend} />
+      ))}
+    </div>
+  );
+};
+
+export default FriendList;
---
+++ src/components/TransactionHistory/TransactionItem/TransactionItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './TransactionItem.module.css';
+
+const TransactionItem = ({ type, amount, currency }) => {
+  return (
+    <tr className={s.transactionRow}>
+      <td className={s.cell}>{type}</td>
+      <td className={s.cell}>{amount}</td>
+      <td className={s.cell}>{currency}</td>
+    </tr>
+  );
+};
+
+export default TransactionItem;
